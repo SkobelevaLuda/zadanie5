e3a1e90f4555b6f39d02674c809d41007541c334
Людок <skobeleva.ludmila@yandex.ru>
2022-10-30 16:21:53 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задание 1
+
+        System.out.println( " Задание 1 ");
+
+        int age = 16;
+        if (age >=18) {
+            System.out.println( " Поздавляем с совершеннолетием! ");
+        }
+        if (age <18 ){
+            System.out.println( "Возраст совершеннолетия ещё не наступил, нужно немного подождать." );
+        }
+
+    }
+
+}
